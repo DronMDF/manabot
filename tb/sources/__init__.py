51a34b07e9c480abafd0956cd1abbce3fa6d3b9b
Andrey Valyaev <dron.valyaev@gmail.com>
2017-12-03 22:59:18 +0300

    #89: Ограничение доступа для определенных команд только для админа

--- tb/sources/__init__.py
+++ tb/sources/__init__.py
@@ -1,7 +1,7 @@
 from .cpu import SoCPU
 from .gerrit import *
 from .hdd import SoHDD
-from .reaction import ReactionEcho
+from .reaction import *
 from .syslog import SoSyslog
 from .telegram import *
 from .utility import SoJoin, SoSafe
