941a1474cd48e98f8b1a976c5378161644fa6f98
Andrey Valyaev <dron.valyaev@gmail.com>
2017-12-21 23:34:32 +0300

    #55: Maintainability index

---
+++ tb/sources/action.py
@@ -0,0 +1,30 @@
+class AcTelegramText:
+	def __init__(self, update, text):
+		self.update = update
+		self.text = text
+
+	def save(self, db):
+		db.set('update_id', self.update.update_id, 'telegram')
+
+	def send(self, transport):
+		transport.sendMessage(
+			chat_id=self.update.effective_chat.id,
+			text=self.text
+		)
+
+
+class AcAdminAction:
+	def __init__(self, update_id, action, review_id):
+		self.update_id = update_id
+		self.action = action
+		self.review_id = review_id
+
+	def save(self, db):
+		db.insert({
+			'action': self.action,
+			'review_id': self.review_id
+		}, 'admin')
+		db.set('update_id', self.update_id, 'telegram')
+
+	def send(self, transport):
+		pass
--- tb/sources/reaction.py
+++ tb/sources/reaction.py
@@ -1,5 +1,6 @@
 import hashlib
 import re
+from .action import *
 
 
 class ReactionRestrict:
@@ -30,21 +31,6 @@ class ReactionChoiced:
 				return r.react(update)
 
 
-class AcTelegramText:
-	def __init__(self, update, text):
-		self.update = update
-		self.text = text
-
-	def save(self, db):
-		db.set('update_id', self.update.update_id, 'telegram')
-
-	def send(self, transport):
-		transport.sendMessage(
-			chat_id=self.update.effective_chat.id,
-			text=self.text
-		)
-
-
 class ReactionAlways:
 	def __init__(self, text):
 		self.text = text
@@ -54,23 +40,6 @@ class ReactionAlways:
 
 	def react(self, update):
 		return AcTelegramText(update, self.text)
-
-
-class AcAdminAction:
-	def __init__(self, update_id, action, review_id):
-		self.update_id = update_id
-		self.action = action
-		self.review_id = review_id
-
-	def save(self, db):
-		db.insert({
-			'action': self.action,
-			'review_id': self.review_id
-		}, 'admin')
-		db.set('update_id', self.update_id, 'telegram')
-
-	def send(self, transport):
-		pass
 
 
 class ReactionReview:
